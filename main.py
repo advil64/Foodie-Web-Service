e516e7b3cd6cd561fa006dac86868d5fcda39e9b
Advith Chegu <advil64@gmail.com>
2021-10-27 14:25:40 -0400

    finished service need to test edge cases

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_restx import Resource, Api, fields
 from flask_cors import CORS
 import json
 from flask_ngrok import run_with_ngrok
+from .queries import get_geocode, find_food
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -19,43 +20,33 @@ api = Api(
 
 app.register_blueprint(blueprint)
 
-GET_TWITTER_INFO = api.model("UserInfo", {
-    "username": fields.String(),
-    "profile_url": fields.String(),
-    "bio": fields.String(),
-    "date_created": fields.DateTime(dt_format="iso8601"),
-    "display_name": fields.String(),
-    "vectors": fields.List(fields.String()),
-    "scores": fields.List(fields.Float())})
+BUSINESS_MODEL = api.model("Business", {
+    'name': fields.String(required=True),
+    'address': fields.String(required=True),
+    'rating': fields.Float(required=True),
+})
+
+RESTAURANT_RESULTS_MODEL = api.model("Restaurants", {
+    'restaurants': fields.List(fields.Nested(BUSINESS_MODEL)),
+})
 
 # documentation for swagger UI
 restaurants = api.namespace(
     "restaurants", description="Returns list of nearby restaurants given an address"
 )
 
-
 @restaurants.route("")
 class GetUserAnalytics(Resource):
     """
     Returns list of restaurants
     """
-
     @api.param(
         "Address",
-        description="The address you would like to find restaurants near",
+        description="The address you would like to find restaurants near (Ex. 3425 Stone Street, Apt. 2A, Jacksonville, FL 39404)",
         type="string",
     )
-    # @api.marshal_with(GET_TWITTER_INFO, mask=None)
+    @api.marshal_with(RESTAURANT_RESULTS_MODEL, mask=None)
     def get(self):
-        # user's screen name
-        address = request.args.get("Address")
-        # vec = getTwitterTimeline(request.args.get("Username"))
-        # print(list(vec.values()))
-        # return {"username": user.screen_name,
-        #         "profile_url": user.profile_image_url,
-        #         "bio": user.description,
-        #         "date_created": user.created_at,
-        #         "display_name": user.name,
-        #         "vectors": list(vec.keys()),
-        #         "scores": list(vec.values())}
-        return {"Address": address}
+        lat, long = get_geocode(request.args.get("Address"))
+        results = find_food(lat, long)
+        return {'restaurants': results}
---
+++ queries.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+import requests
+import pprint
+from flask import abort
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+def get_geocode(address):
+    p = {'q': address, 'api_key': os.environ.get("geocode-key")}
+    res = requests.get('https://api.geocod.io/v1.6/geocode', params=p)
+
+    # parse data as json and retrieve lat and long
+    data = res.json()
+    # pp.pprint(data)
+    if data['results']:
+        lat = data['results'][0]['location']['lat']
+        lng = data['results'][0]['location']['lng']
+    else:
+        abort(400, "Invalid address")
+    
+    return lat, lng
+
+def find_food(lat, lng):
+    p = {'latitude': lat, 'longitude': lng, 'categories': 'Food'}
+    h = {'Authorization': 'Bearer {}'.format(os.environ.get("yelp-key"))}
+    res = requests.get('https://api.yelp.com/v3/businesses/search', params=p, headers=h)
+
+    # parse our response and return
+    data = res.json()
+    business_info = []
+    for b in data['businesses']:
+        business_info.append({'name': b['name'], 'address': ' '.join(b['location']['display_address']), 'rating': b['rating']})
+    return business_info
